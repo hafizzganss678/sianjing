88d68b2e167c06be51290b4364d361798f378222
Hafizz <81644042+hafizzganss678@users.noreply.github.com>
2022-10-20 14:04:20 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,156 @@
+exports.allmenu = (prefix) => {
+return `▻►▻►▻►▻►▻►▻►▻►▻►
+╭────[ *MAIN MENU* ]
+│ ✨${prefix}db
+│ ✨${prefix}tes
+│ ✨${prefix}dev
+│ ✨${prefix}owner
+│ ✨${prefix}runtime
+│ ✨${prefix}dashboard
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *OTHER MENU* ]
+│ ✨${prefix}ping
+│ ✨${prefix}simi
+│ ✨${prefix}rules
+│ ✨${prefix}listpc
+│ ✨${prefix}listgc
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *FUN MENU* ]
+│ ✨${prefix}jadian
+│ ✨${prefix}jodohku
+│ ✨${prefix}confes
+│ ✨${prefix}menfes
+│ ✨${prefix}rate
+│ ✨${prefix}apakah
+│ ✨${prefix}bisakah
+│ ✨${prefix}siapakah
+│ ✨${prefix}bagaimanakah
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *TOOLS MENU* ]
+│ ✨${prefix}tts
+│ ✨${prefix}isgd
+│ ✨${prefix}cuttly
+│ ✨${prefix}tinyurl
+│ ✨${prefix}nulis
+│ ✨${prefix}toimg
+│ ✨${prefix}obfus
+│ ✨${prefix}ssweb
+│ ✨${prefix}sticker
+│ ✨${prefix}styletext
+│ ✨${prefix}translate
+│ ✨${prefix}kalkulator
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *BAILEYS MENU* ]
+│ ✨${prefix}afk
+│ ✨${prefix}react
+│ ✨${prefix}fitnah
+│ ✨${prefix}hidetag
+│ ✨${prefix}towame
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *SET PROSES/DONE* ]
+│ ✨${prefix}setdone
+│ ✨${prefix}delsetdone
+│ ✨${prefix}changedone
+│ ✨${prefix}setproses
+│ ✨${prefix}delsetproses
+│ ✨${prefix}changeproses
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *SET WELCOME/LEFT* ]
+│ ✨${prefix}setleft
+│ ✨${prefix}delleft
+│ ✨${prefix}getleft
+│ ✨${prefix}changeleft
+│ ✨${prefix}setwelcome
+│ ✨${prefix}delwelcome
+│ ✨${prefix}getwelcome
+│ ✨${prefix}changewelcome
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *STORE MENU* ]
+│ ✨${prefix}list
+│ ✨${prefix}addlist
+│ ✨${prefix}dellist
+│ ✨${prefix}done
+│ ✨${prefix}proses
+│ ✨${prefix}kali
+│ ✨${prefix}bagi
+│ ✨${prefix}kurang
+│ ✨${prefix}tambah
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *GROUP MENU* ]
+│ ✨${prefix}left
+│ ✨${prefix}add
+│ ✨${prefix}kick
+│ ✨${prefix}open
+│ ✨${prefix}close
+│ ✨${prefix}group
+│ ✨${prefix}tagall
+│ ✨${prefix}delete
+│ ✨${prefix}revoke
+│ ✨${prefix}antilink
+│ ✨${prefix}welcome
+│ ✨${prefix}hidetag
+│ ✨${prefix}demote
+│ ✨${prefix}setdesc
+│ ✨${prefix}linkgrup
+│ ✨${prefix}promote
+│ ✨${prefix}setppgrup
+│ ✨${prefix}setnamegc
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *OWNER MENU* ]
+│ ✨${prefix}bc
+│ ✨${prefix}join
+│ ✨${prefix}leave
+│ ✨${prefix}block
+│ ✨${prefix}resetdb
+│ ✨${prefix}unblock
+│ ✨${prefix}creategc
+│ ✨${prefix}sendsesi
+│ ✨${prefix}setppbot
+│ ✨${prefix}setbiobot
+│ ✨${prefix}broadcast
+│ ✨${prefix}restart
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *STALKER MENU* ]
+│ ✨${prefix}igstalk
+│ ✨${prefix}ffstalk
+│ ✨${prefix}mlstalk
+│ ✨${prefix}npmstalk
+│ ✨${prefix}githubstalk
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *VOKAL MENU* ]
+│ ✨${prefix}hilih
+│ ✨${prefix}huluh
+│ ✨${prefix}heleh
+│ ✨${prefix}holoh
+│ ✨${prefix}halah
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *INFORMATION* ]
+│ ✨${prefix}cuaca
+│ ✨${prefix}kodepos
+│ ✨${prefix}infogempa
+│ ✨${prefix}covidworld
+╰─────────────────
+▻►▻►▻►▻►▻►▻►▻►▻►
+╭───[ *DOWNLOADER* ]
+│ ✨${prefix}play
+│ ✨${prefix}tiktok
+│ ✨${prefix}ytmp3
+│ ✨${prefix}ytmp4
+│ ✨${prefix}gitclone
+│ ✨${prefix}mediafire
+│ ✨${prefix}tiktokmp3
+│ ✨${prefix}tiktoknowm
+╰─────────────────`
+}
